e03dc960870fc8bd8cc1dd5039b0e8fc8f52edd5
Trey Villafane <tvillafane1990@gmail.com>
2014-10-05 15:41:41 -0700

    fixed IOS multiple photo bug

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,19 +7,27 @@ class PicturesController < ApplicationController
     @picture = current_user.pictures.new
   end
 
-  def create
+  def create 
     # render :text => params.inspect
     if !params[:images].nil? 
+      count = 0
       params[:images].each do |img|
-        User.find(params[:user_id]).pictures.create(:avatar => img[:avatar])
-      end
+        # iOS upload bug fix
+        temp = img[:avatar].original_filename.split('.')
+        if  temp[0] == 'image'
+          img[:avatar].original_filename = "iOS" + Time.now.to_i.to_s + "number" + count.to_s + "user" + current_user.id.to_s 
+          count += 1
+        end  
+      # render :text => params[:images].inspect
+      User.find(params[:user_id]).pictures.create(:avatar => img[:avatar])
+     end  #loop
       flash[:message] = "Successfully added #{params[:images].length} photos!"
       redirect_to user_pictures_path current_user
     else
       flash[:errors] = 'Please include at least one image'
       redirect_to user_pictures_path current_user
     end
-  end
+  end  #fn
 
   def destroy
     params[:delete_ids].each do |id|
